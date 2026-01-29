f301bda205068c91764f9a083de641661a39feee
Mkn0021 <nayeem21122002@gmail.com>
2026-01-29 14:37:28 +0600

    feat(backend): add api route for user/me  - GET: users own profile  - PUT: update profile  -DELETE: delete profile

---
+++ app/(user)/api/user/me/route.ts
@@ -0,0 +1,49 @@
+import APIError from '@/lib/api/error';
+import { auth } from '@/app/(auth)/auth';
+import { NextRequest } from 'next/server';
+import { asyncHandler } from '@/lib/api/response';
+import { UpdateMeSchema } from '@/app/(user)/api/schema';
+import { deleteUserById, getUserById, updateUser } from '@/app/(user)/api/action';
+
+
+// GET - api/user/me
+export const GET = asyncHandler(
+    async (_, context) => {
+
+        const userRecord = await getUserById(context.session!.user.id);
+        if (!userRecord) throw APIError.notFound("User not found");
+
+        return {
+            data: userRecord,
+            message: 'User retrieved successfully'
+        };
+    }
+);
+
+// PUT - api/user/me
+export const PUT = asyncHandler(
+    async (_, context, data) => {
+
+        const userRecord = await updateUser(context.session!.user.id, data.body);
+
+        return {
+            data: userRecord,
+            message: 'User updated successfully'
+        };
+    },
+    UpdateMeSchema
+);
+
+
+// DELETE - api/user/me
+export const DELETE = asyncHandler(
+    async (req: NextRequest, context) => {
+
+        await deleteUserById(context.session!.user.id);
+        await auth.api.signOut({ headers: req.headers });
+
+        return {
+            message: 'User deleted successfully'
+        };
+    }
+);
