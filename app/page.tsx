7db3ae1b0c39aab33cf5f5fec26b60bf7a6ec82a
Mkn0021 <nayeem21122002@gmail.com>
2026-01-30 12:52:47 +0600

    feat(ui): add homepage combining all sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import { Navbar } from "@/components/homepage/navbar";
+import { HeroSection } from "@/components/homepage/hero-section";
+import { BrandSection } from "@/components/homepage/brand-section";
+import { StatSection } from "@/components/homepage/stat-section";
+import { FeatureSection } from "@/components/homepage/feature-section";
+import { Footer } from "@/components/homepage/footer";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-6xl">Hello from Next-JS</h1>
+    <div className="flex flex-col gap-8 min-h-screen items-center justify-center font-sans">
+      <Navbar />
+      <HeroSection />
+      <BrandSection />
+      <StatSection />
+      <FeatureSection />
+      <Footer />
     </div>
   );
 }
