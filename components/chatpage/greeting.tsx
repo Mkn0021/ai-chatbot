971f7d7971f5a2051ee6752f4cb363af443482b9
Mkn0021 <nayeem21122002@gmail.com>
2026-02-03 02:21:00 +0600

    feat(ui/greeting): enhance greeting text styling for improved visual impact

--- components/chatpage/greeting.tsx
+++ components/chatpage/greeting.tsx
@@ -8,16 +8,17 @@ export const Greeting = () => {
         >
             <motion.div
                 animate={{ opacity: 1, y: 0 }}
-                className="font-semibold text-xl md:text-2xl"
+                className="text-4xl md:text-6xl font-bold"
                 exit={{ opacity: 0, y: 10 }}
                 initial={{ opacity: 0, y: 10 }}
                 transition={{ delay: 0.5 }}
             >
-                Hello there!
+                Hello
+                <span className="text-[#FF7757]"> there!</span>
             </motion.div>
             <motion.div
                 animate={{ opacity: 1, y: 0 }}
-                className="text-xl text-zinc-500 md:text-2xl"
+                className="text-2xl text-gray-500 md:text-4xl"
                 exit={{ opacity: 0, y: 10 }}
                 initial={{ opacity: 0, y: 10 }}
                 transition={{ delay: 0.6 }}
